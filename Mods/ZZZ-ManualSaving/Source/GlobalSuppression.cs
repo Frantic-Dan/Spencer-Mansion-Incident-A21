3291d305db67271231932efa0609e58aaa46021b
Frantic-Dan <39160593+Frantic-Dan@users.noreply.github.com>
2024-02-09 13:20:35 +0800

    Create GlobalSuppression.cs

---
+++ Mods/ZZZ-ManualSaving/Source/GlobalSuppression.cs
@@ -0,0 +1,21 @@
+using System.Diagnostics.CodeAnalysis;
+
+// Perpetually annoying and pointless things.
+[assembly: SuppressMessage("Style", "IDE0160")] // No file namespacing.
+[assembly: SuppressMessage("Style", "IDE0040")] // Add access modifiers
+[assembly: SuppressMessage("Style", "IDE0055")] // No Tab indents
+[assembly: SuppressMessage("Style", "IDE0008")] // No var
+[assembly: SuppressMessage("Style", "IDE0022")] // No arrow fn syntax.
+[assembly: SuppressMessage("Style", "IDE0058")] // Expr is never used.
+[assembly: SuppressMessage("Style", "IDE0072")] // Expand switch expressions.
+[assembly: SuppressMessage("Style", "IDE0051")] // Method is unused.
+[assembly: SuppressMessage("Style", "IDE0061")] // Use method body, not arrow.
+
+// C#12 collection initialiser recommend (remove later).
+[assembly: SuppressMessage("Style", "IDE0028")]
+
+// C#12 collection initialiser recommend (remove later).
+[assembly: SuppressMessage("Style", "IDE0300")]
+
+// C#12 primary ctor recommend (remove later).
+[assembly: SuppressMessage("Style", "IDE0290")]
